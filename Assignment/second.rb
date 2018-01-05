e44039f08216b7c8a134c22904ebbfb9c99e4a4d
vishal-bacancy <vishal.jariwala@bacancytechnology.com>
2018-01-05 18:25:51 +0530

    second.rb upated

--- Assignment/second.rb
+++ Assignment/second.rb
@@ -1,12 +1,19 @@
 puts "Enter room length : "
-l = gets.to_i
+l = gets.to_f
 puts "Enter room width : "
-w = gets.to_i
+w = gets.to_f
 
 tot = l*w
+block = 0.0
+block = tot.to_f / 144
 
-block = tot /144
+b = block.to_s.split('.')
+no = b[1].to_i
+rem = block - no
+
+
+
 puts "total block required are #{block}"
 
-cost = (tot*30)+(tot*2)
+cost = (no*30)+(no*2)+(rem*30)+(rem*2)
 puts "total cost is #{cost}"
