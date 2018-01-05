85e7489531798027e30b9c1e848f2e289c7bc208
vishal-bacancy <vishal.jariwala@bacancytechnology.com>
2018-01-05 12:35:27 +0530

    final modification

--- Assignment/sixth.rb
+++ Assignment/sixth.rb
@@ -82,30 +82,55 @@ else
 
 end
 
-def sort_array(arr)
-minv = 0
-mink = ""
-i=1
-r=0
-tempk=""
-tempv=0
-minv = arr[0].to_h.values
-mink = arr[0].to_h.keys
-while i<arr.size
-if(min>arr[i].to_h.values)
-	tempk = arr[i].to_h.keys
-	tempv  = arr[i].to_h.values
-	arr[i].to_h.keys = mink
-	arr[i].to_h.values = minv
+def sort_hash(hj)
+
+akeys = []
+avalues = []
+for o in 0..hj.size-1
+akeys[o] = hj.keys[o]
+avalues[o] = hj[hj.keys[o]]
 
 
 
 end
 
 
+for i in 0..avalues.size-1
+
+for j in 0..avalues.size-i-1
+if(j+1 > hj.size-1)
+	break 
 end
 
+if(avalues[j] < avalues[j+1])
 
- cd = array.sort_by{|first,second| second}.reverse
+temp = avalues[j]
+tempk = akeys[j]
 
-  puts "highest tempreture is "+cd.to_h.keys[0]+" in "+cd.to_h.values[0].to_s
+avalues[j] = avalues[j+1]
+akeys[j] = akeys [j+1]
+
+avalues[j+1] = temp
+akeys[j+1] = tempk
+
+end
+
+end
+end
+
+h = Hash.new
+for o in 0..akeys.size-1
+h[akeys[o]]  = avalues[o]
+
+
+end
+
+return h
+
+
+end
+
+cd = sort_hash(array)
+ #cd = array.sort_by{|first,second| second}.reverse
+
+  puts "highest tempreture is "+cd.keys[0]+" in "+cd.values[0].to_s
